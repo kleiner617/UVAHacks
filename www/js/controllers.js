91e8007914a9a549def958b8a50963d10a53a95b
Micah Parks <micahleviparks@gmail.com>
2017-03-26 11:19:11 -0400

    Added FavLocs within 5km radius on map call.
    March 26th 11:18

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -82,21 +82,68 @@ angular.module('starter.controllers', [])
 
 
 .controller('MapCtrl', function($scope, $state, $cordovaGeolocation) {
+
+  var testz = {'A': [38.037512,-78.515489, 'how you', 'how many yums'], 'B': [38.030211,-78.514395, 'chums', 'i eat all the yums'], 'C': [38.044171,-78.464699, 'taco tuesday', 'bring on the yums!'], 'D': [38.027574,-78.506327, 'jumbo', 'ony after rain']}
+
   var options = {timeout: 10000, enableHighAccuracy: false};
 
   $cordovaGeolocation.getCurrentPosition(options).then(function(position){
 
-    var startLatLng = new google.maps.LatLng(43.07493,-89.381388);
+    var startLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+    var startLatLngBoth = [position.coords.latitude, position.coords.longitude];
 
     var mapOptions = {
       center: startLatLng,
       zoom: 15,
       mapTypeId: 'hybrid'
     };
-
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var indexFavLocs;
+    var lat1 = position.coords.latitude;
+    var lon1 = position.coords.longitude;
+    nearFavLocs = [];
+    for (var key in testz)
+    {
+        var lat2 = testz[key][0];
+        var lon2 = testz[key][1];
+        var nowFavLocsLatLng = new google.maps.LatLng(lat2, lon2);
+        testz[key][2] = nowFavLocsLatLng;
+        console.log(testz[key][2]);
+        // Start distance finder.
+        var R = 6371; // Radius of the earth in km
+        var dLat = (lat2 - lat1) * (Math.PI / 180);
+        var dLon = (lon2 - lon1) * (Math.PI / 180);
+        var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1) * (Math.PI / 180) * Math.cos(lat2) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c; // Distance in km
+        // End distance finder.
+        if (d <= 5) // About 3.1 miles.
+        {
+            // google.maps.event.addListenerOnce($scope.map, 'idle', function(){
+                (function(){
+                    var marker = new google.maps.Marker({
+                        map: $scope.map,
+                        // animation: google.maps.Animation.DROP,
+                        position: testz[key][2]
+                    })
+                        var infoWindow = new google.maps.InfoWindow({
+                        content: testz[key][3]
+                    })
+
+                    google.maps.event.addListener(marker, 'click', function () {
+                        infoWindow.open($scope.map, marker);
+                    });
+                })();
+
+        //   });
+        };
+    };
 
   }, function(error){
-    console.log('Could not get location');
+    console.log('Unable to grab user location.');
   });
 });
